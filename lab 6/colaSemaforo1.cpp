6e1e84927a42dda94aadb94fcd8d9379050a9eb0
Nelson <gar22434@uvg.edu.gt>
2023-10-06 12:33:20 -0600

    lab 6 terminado

--- lab 6/colaSemaforo1.cpp
+++ lab 6/colaSemaforo1.cpp
@@ -44,14 +44,19 @@ sem_t espacios_en_cola, datos_en_cola;
 */
 int Escribe(int DATO)
 {
+	//Espera a que haya espacios en la cola para escribir (Comienza vacia con 10 espacios libres)
+	//Consume 1 espacio de los 10 disponibles
 	sem_wait(&espacios_en_cola);
 	
+	//Con mutex bloquea el buffer para que no haya problemas
 	pthread_mutex_lock(&m);
 	buffer[in]= DATO;
 	in = (in+1) % 10;
+	//Desbloqueamos el buffer
 	pthread_mutex_unlock(&m);
 	
 	/*----- desbloquea el semaforo de lleno -----*/
+	//Hacemos un post para ocupar un espacio en el semaforo (datos en cola)
 	sem_post(&datos_en_cola);
 } 
 
@@ -63,6 +68,7 @@ int Escribe(int DATO)
  void *productor(void * arg){ 
   int i; 
   //printf("Inicia ejecución de Hilo 1\n");
+  //Itera 100 veces la funcion escribe
   for (i= 0; i< 100; i++) 
     Escribe(i); 
   pthread_exit(0); 
@@ -76,16 +82,22 @@ int Escribe(int DATO)
 int Lee()
 {
 	int dato; 
+	//Condicion para escribir "cola vacia"
 	if((out)%10==0)
 		printf("<<<Cola vacia \n");
+	//Esperamos a que haya datos en la cola para leer
+	//Consume 1 elemento de datos en cola
 	sem_wait(&datos_en_cola);
 	
+	//Una vez que hay datos en la cola, bloqueamos el buffer para extraer el dato
 	pthread_mutex_lock(&m);	
 	dato = buffer[out]; 
 	out = (out+1) % 10;
+	//Desbloqueamos el buffer
 	pthread_mutex_unlock(&m);
 	
 	/*---- desbloquea el semaforo de vacio -----*/
+	//Hacemos un post para liberar un espacio en el semaforo (espacios en cola)
 	sem_post(&espacios_en_cola);
   return dato; 
 } 
@@ -107,10 +119,10 @@ int main(int argc, char *argv[])
 	sem_init(&datos_en_cola,0,0);
 	sem_init(&espacios_en_cola,0,10);
 	
+	//Creamos el hilo hijo que ejecutará la funcion productor
 	pthread_create(&hijo,NULL,productor,NULL);
-	//usleep(1000);
-	//printf("Continua ejecución Hilo Principal\n");
-	
+
+	//El hilo principal itera 100 veces la funcion lee para leer los datos del buffer
 	for (i= 0; i< 100; i++)
 	{
 		printf("%d ", Lee());
@@ -119,3 +131,5 @@ int main(int argc, char *argv[])
 	}
 	exit(0); 
 }
+
+//Compile with: gcc colaSemaforo1.cpp -o colaSemaforo1 -lpthread
--- lab 6/cond_var2.cpp
+++ lab 6/cond_var2.cpp
@@ -30,16 +30,19 @@ pthread_cond_t lleno, vacio;
 pthread_mutex_t mutex; 
 
 int Escribe(int DATO){
-	
+	// se bloquea el mutex
   pthread_mutex_lock (&mutex); 
   
   while (cont == 10) 
+  // se bloquea el hilo hasta que se cumpla la condición
     pthread_cond_wait(& vacio, &mutex); 
   cont++; 
   
   buffer[in]= DATO;
   in = (in+1) % 10;    // 0 0  1 1  para todos valores menor igual 10... indice 1 valor num 11, ind 2 vn 12...
+  // se despierta a todos los hilos que estén esperando por la variable de condición lleno
   pthread_cond_broadcast(& lleno); 
+  // se desbloquea el mutex
   pthread_mutex_unlock(& mutex); 
   
   return 0;
@@ -48,25 +51,33 @@ int Escribe(int DATO){
 int Lee(){ 
 
   int dato;   
+  // se bloquea el mutex
   pthread_mutex_lock(&mutex);
   // IMPORTANTE: Se emplea un ciclo, no un if
-  while (cont == 0) 
+  while (cont == 0)
+    // se bloquea el hilo hasta que se cumpla la condición 
     pthread_cond_wait(& lleno, &mutex); 
   cont--; 
   
   dato = buffer[out]; 
   out = (out+1) % 10;
+  // se despierta a todos los hilos que estén esperando por la variable de condición vacio
   pthread_cond_broadcast(& vacio); 
+  // se desbloquea el mutex
   pthread_mutex_unlock(&mutex); 
   return dato; 
 } 
+
  void *productor(void * arg){ 
  
   int i; 
   printf("Hilo hijo trabajando...\n");
+  // for que ejecuta la función Escribe 20 veces
   for (i= 0; i< 20; i++)
 	Escribe(i); 
+  // se hace un sleep para que el hilo hijo pueda trabajar
   usleep(1000);
+  // sale del hilo
   pthread_exit(0); 
   
   return NULL;
@@ -77,16 +88,22 @@ int main(){
   pthread_t hijo;
   in = out = cont = 0;
 
+// Inicialización del mutex
   pthread_mutex_init(&mutex, NULL); 
+// Inicialización de las variables de condición
   pthread_cond_init(&lleno, NULL); 
   pthread_cond_init(&vacio, NULL);
-  
+// Creación del hilo, se le pasa la función productor
   pthread_create(&hijo,NULL,productor,NULL);
-  
+  // se hace un sleep para que el hilo hijo pueda trabajar
   usleep(1000);
   printf("Proceso Principal\n");
-  
+  // for que ejecuta la función Lee 20 veces
   for (i= 0; i< 20; i++) 
+  //se imprime el valor que retorna la función Lee
     printf("%d\n ", Lee());
+
   exit(0); 
 }
+
+// compilar: gcc -o cond_var2 cond_var2.cpp -lpthread
